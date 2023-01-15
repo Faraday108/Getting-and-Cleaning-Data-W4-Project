08f7c1496fc1aa3fdea905387806f23161d96a93
Faraday108 <youngnt225@gmail.com>
2023-01-15 14:39:37 -0500

    Removed data import and placed in dedicated file titled import_data.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,22 +7,6 @@
 #     with the average of each variable for each activity and each subject.
 
 ### Import Data ###
-#Import test data
-X_test <- read.table("./data/Dataset/UCI HAR Dataset/test/X_test.txt",sep = "\t",
-                   header = FALSE)
-# Test labels, see activity_labels to link to activity name
-Y_test <- read.table("./data/Dataset/UCI HAR Dataset/test/y_test.txt", sep = "\t",
-                   header = FALSE)
-# Import train data
-X_train <- read.table("./data/Dataset/UCI HAR Dataset/train/X_train.txt", sep = "\t",
-                      header = FALSE)
-# Train labels, see activity_labels to link to activity name
-Y_train <- read.table("./data/Dataset/UCI HAR Dataset/train/y_train.txt", sep = "\t",
-                      header = FALSE)
+# If not already done, import data using import_data.R. 
 
-# Load data linking class label to activity name
-activity_labels <- read.table("./data/Dataset/UCI Har Dataset/activity_labels.txt")
-# Load data linking row to subject who performed the activity for each window sample. Range 1 to 30
-subject_train <- read.table("./data/Dataset/UCI HAR Dataset/train/subject_train.txt", sep ="")
-# Load data linking row to subject who performed the activity for each window sample. Range 1 to 30
-subject_test <- read.table("./data/Dataset/UCI HAR Dataset/test/subject_test.txt", sep ="")
+
